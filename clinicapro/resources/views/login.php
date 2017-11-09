152dbd28a1c9e7cf87289a05c7c4dcab2811cad2
tunime <almaseb_123@hotmail.com>
2017-11-09 16:03:21 -0500

    esta es una subida de prueva

--- clinicapro/resources/views/login.php
+++ clinicapro/resources/views/login.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>login</title>
+	<title>loginn</title>
 	<link rel="stylesheet" href="css/base.css">
 	<link rel="stylesheet" href="css/main.css">
 </head>
