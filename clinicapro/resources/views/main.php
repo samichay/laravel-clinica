39be14cf97808bdbd3c5382875bf3e135dcd55a7
tunime <almaseb_123@hotmail.com>
2017-11-10 15:04:47 -0500

    Incorporando el menu del giancarlitos e ingresando una nueva arquitectura

---
+++ clinicapro/resources/views/main.php
@@ -0,0 +1,62 @@
+<!doctype html>
+
+<html lang="es">
+	<head>
+		<title></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<link rel="stylesheet" type="text/css" href="css/menult.css" />
+        <link rel="stylesheet" href="css/base.css">
+	</head>
+
+	<body>
+        <!-- inicio del menu lateral-->
+		<div id="barra-lateral">
+			<div id="logo-menu">
+			</div>
+
+			<div id="contenedor-logos-primero" class="contenedor-logos">
+				<div class="logo">
+				</div>
+				<p class="title-menu">Inicio</p>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+				</div>
+				<p class="title-menu">Registro</p>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+				</div>
+				<p class="title-menu">Consultas</p>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+				</div>
+				<p class="title-menu">Reportes</p>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+				</div>
+				<p class="title-menu">Estadisticas</p>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+				</div>
+				<p class="title-menu">Ayuda</p>
+			</div>
+		</div>
+        <!-- fin del menu lateral -->
+        <!-- inicio del contenido -->
+        <div>
+        </div>
+        <!-- finb del contenido -->
+
+
+
+	</body>
+</html>
