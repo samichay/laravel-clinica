673ed4b1d1d51c340d47e8c8facca0e4e0d35994
AlvaroMoCa <almaseb_123@hotmail.com>
2017-11-22 16:49:34 -0500

    Añadiendo la vista del registro

--- clinicapro/app/Http/routes.php
+++ clinicapro/app/Http/routes.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+Route::get('/registropaciente',function(){
+	return view('registrop');
+});
 
---
+++ clinicapro/resources/views/registrop.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<title></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<link rel="stylesheet" type="text/css" href="css/menult.css" />
+        <link rel="stylesheet" href="css/base.css">
+        <link rel="stylesheet" href="css/admin.css">
+	</head>
+
+	<body>
+        <!-- inicio del menu lateral-->
+		<div id="barra-lateral">
+			<div id="logo-menu">
+			</div>
+
+			<div id="contenedor-logos-primero" class="contenedor-logos">
+				<div class="logo">
+					<a href="#" class="title-menu">Inicio</a>
+				</div>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+					<a href="#" class="title-menu">Registro</a>
+				</div>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+					<a href="#" class="title-menu">Consultas</a>
+				</div>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+					<a href="#" class="title-menu">Reportes</a>
+				</div>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+					<a href="#" class="title-menu">Estadisticas</a>
+				</div>
+			</div>
+
+			<div class="contenedor-logos">
+				<div class="logo">
+					<a href="#" class="title-menu">Ayuda</a>	
+				</div>
+			</div>
+		</div>
+        <!-- fin del menu lateral -->
+        <!-- inicio del contenido -->
+        <div class="contenido">
+            <div class="contenido--tabla">
+                <center>
+                    <h2 class="contenido--titulo">
+                        Registro de paciente
+                    </h2>
+                    
+                </center>
+            </div>
+        </div>
+        <!-- finb del contenido -->
+
+
+
+	</body>
+</html>
