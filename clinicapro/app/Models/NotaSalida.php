34c1d0cd4fe531f71d3c8aeda4018587e9befae0
Pvnn17 <angel13_x@hotmail.com>
2017-11-24 16:14:54 -0500

    nuevos modelos

---
+++ clinicapro/app/Models/NotaSalida.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NotaSalida extends Model
+{
+    protected $table ='atencion';
+    public $timestamps =false;
+}
---
+++ clinicapro/app/Models/Tipo_Atencion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tipo_Atencion extends Model
+{
+     protected $table ='atencion';
+    public $timestamps =false;
+}
