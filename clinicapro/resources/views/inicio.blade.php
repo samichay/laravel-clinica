b556246860cd6652b88091645634c5eef8d20895
tunime.1@gmail.com <tunime.1@gmail.com>
2017-12-03 06:45:45 -0500

    Diseño del inicio

--- clinicapro/resources/views/inicio.blade.php
+++ clinicapro/resources/views/inicio.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts/admin')
 
 @section('body')
-<h1>Pagina de inicio</h1>
+<div class="targetas">
+    <a href="#" class="targeta-uno">
+        <h2>03</h2>
+        <p>Citas pendientes</p>
+    </a>
+    <a href="#" class="targeta-dos">
+        <h2>09</h2>
+        <p>Isumos Agotados</p>
+    </a>
+</div>
 @endsection
